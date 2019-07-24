329e6964dd4f339c171a462faf45baed6a2a5c5a
Kolbasin Pavel <to.kolbasin@gmail.com>
2019-07-24 22:07:24 +0300

    l12hw71%Modification2

--- lesson12/UkrainianBankSystem.java
+++ lesson12/UkrainianBankSystem.java
@@ -2,82 +2,76 @@ package lesson12;
 
 public class UkrainianBankSystem implements BankSystem {
 
-
     @Override
     public void withdraw(User user, int amount) {
-        //проверить, можно ли снять - проверить лимит и достаточно ли денег
-        //снять
- /*     int limitOFWithdrawal = amount + user.getBank().getCommission(amount);
-      if(amount + user.getBank().getCommission(amount)> limitOFWithdrawal){
-         prinWithdrawalErrorMsg(amount, user);
-          return;
-      }
-        if(amount + user.getBank().getCommission(amount)> user.getBalance()){
-            prinWithdrawalErrorMsg(amount, user);
+
+        if (!checkWithdraw(user, amount)) {
             return;
-        }*/
-
-
-        if (!checkWithdraw(user, amount))
-            return;
-        user.setBalance(user.getBalance() - amount - (amount * user.getBank().getCommission(amount)));
-    }
-
-    @Override
-    public void fund(User user, int amount) {
-        if (!checkFund(user, amount))
-            return;
-        user.setBalance(user.getBalance() + amount - amount * user.getBank().getCommission(amount));
-    }
-
-    @Override
-    public void transferMoney(User fromUser, User toUser, int amount) {
-        // снимаем деньги
-        //пополняем
-        if (!checkWithdraw(fromUser, amount) && !checkFund(toUser, amount))
-            return;
-
-
-        fromUser.setBalance(fromUser.getBalance() - amount - amount * fromUser.getBank().getCommission(amount));
-        toUser.setBalance(toUser.getBalance() + amount - amount * toUser.getBank().getCommission(amount));
-
-
-    }
-
-    @Override
-    public void paySalary(User user) {
-        if (user.getSalary() < user.getBank().getLimitOfFunding())
-            return;
-        user.setBalance(user.getBalance() + user.getSalary());
-
+        } else user.setBalance(user.getBalance() - amount - (amount * user.getBank().getCommission(amount)));
     }
 
     private boolean checkWithdraw(User user, int amount) {
-        return (!checkWithdrawLimits(user, amount, user.getBank().getLimitOfWithdrawal())) &&
-                !checkWithdrawLimits(user, amount, user.getBalance());
+        return checkWithdrawLimits(user, amount, user.getBank().getLimitOfWithdrawal()) &&
+                checkWithdrawLimits(user, amount, user.getBalance());
     }
 
-
     private boolean checkWithdrawLimits(User user, int amount, double limit) {
-        if (amount + user.getBank().getCommission(amount) > limit) {
+        if (amount + amount * user.getBank().getCommission(amount) > limit) {
             printWithdrawalErrorMsg(amount, user);
             return false;
         }
         return true;
     }
 
-    private boolean checkFund(User user, int amount) {
-        if (amount - user.getBank().getCommission(amount) > user.getBank().getLimitOfFunding()) {
-            printFundErrorMsg(amount, user);
+    private void printWithdrawalErrorMsg(int amount, User user) {
+        System.err.println("Can't withdraw money " + amount + " from user " + user.toString());
+    }
+
+
+    @Override
+    public void fund(User user, int amount) {
+        if (!checkFunding(user, amount))
+            return;
+        user.setBalance(user.getBalance() + amount);
+    }
+
+    private boolean checkFunding(User user, int amount) {
+        return (checkFundingLimits(user, amount, user.getBank().getLimitOfFunding()));
+    }
+
+    private boolean checkFundingLimits(User user, int amount, double limit) {
+        if (amount > limit) {
+            printFundingErrorMsg(user, amount);
             return false;
         }
         return true;
     }
-    private void printWithdrawalErrorMsg(int amount, User user) {
-        System.err.println("Can't withdraw money" + amount + "from user" + user.toString());
+
+    private void printFundingErrorMsg(User user, int amount) {
+        System.err.println("Can't replenish on " + amount + " of user " + user.toString());
     }
 
-    private void printFundErrorMsg(int amount, User user) {
-        System.err.println("Can't found money" + amount + "to user" + user.toString());
+    @Override
+    public void transferMoney(User fromUser, User toUser, int amount) {
+        if (!checkWithdraw(fromUser, amount)) {
+            return;
+        }
+        if (fromUser.getBank().getCurrency() == toUser.getBank().getCurrency()) {
+
+            withdraw(fromUser, amount);
+        } else return;
+
+
+        fund(toUser, amount);
+
     }
+
+    @Override
+    public void paySalary(User user) {
+        if (user.getSalary() > user.getBank().getLimitOfFunding()) {
+            return;
+        } else user.setBalance(user.getBalance() + user.getSalary());
+    }
+
+
 }
